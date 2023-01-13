e2698ee50092cc59b43b6d3fdbe85cb66ac60609
johnquavi <122548093+johnquavi@users.noreply.github.com>
2023-01-13 11:26:01 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Tortoise)
+})
 basic.showIcon(IconNames.Happy)
